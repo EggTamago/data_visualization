ff08a219275faa9acb32ad8dc96dc4b9a6ac46cf
Tamago <keiichiro.tany@gmail.com>
2021-03-30 13:46:43 +0000

    state class method setState

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
-import React from 'react';
-import Proptypes from 'prop-types';
-
-// function App() {
-
-//   const greeting = "Hello world!";
-//   const dom = <input type="text" onClick={() => {console.log("I am clicked")}} />;
-
-//     return (
-//         dom
-//     );
-// }
-
+import React, {Component} from 'react';
 
 // index.js:1 Warning: Each child in a list should have a unique "key" prop.
 // このwarningみたらkey={index}を足してあげる
 const App = () => {
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 0}
-  ]
   return (
-  <div>
-    {
-    profiles.map((profiles, index) => {
-      return <User name={profiles.name} age={profiles.age} key={index}/>
-    })
-  }
-  </div>
+    <Counter></Counter>
   )
 }
 
+// Propsは親のコンポーネントから値を渡されたのに対して
+// stateはコンポーネントの内部でのみ利用される
+// propsは変更不可能,stateは変更可能
 
-// User componentに対してname, ageという属性(props)が与えられる
-const User = (props) => {
-  return <div>Hi, I am {props.name} and Im {props.age} years old!</div>
+// class書いて、method書いて,最後にrender書いて返す
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = {count : 0}
+  }
+
+  // stateの変更にはsetStateがお決まりのように使う
+  // setStateが呼び出されると自動でrenderされる
+  handlePlusBottun = () => {
+    console.log("handlePlusBottun")
+    this.setState({count : this.state.count + 1})
+  }
+
+  handleMinusBottun = () => {
+    console.log("handleMinusBottun")
+    this.setState({count : this.state.count - 1})
+  }
+
+  render(){
+    console.log("render")
+    return(
+    <React.Fragment>
+      <div>count: {this.state.count}</div>
+      <button onClick={this.handlePlusBottun}>+1</button>
+      <button onClick={this.handleMinusBottun}>-1</button>
+    </React.Fragment>
+    )
+  }
 }
 
-// ここを使ってPropに対する型を定義する
-// isRequiredをつけた場合、ageがなければエラーになる
-User.Proptypes = {
-  name: Proptypes.string,
-  age: Proptypes.number.isRequired
-}
+
 
 
 export default App;
