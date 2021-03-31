4c8e309dfc30d708e9f301bf65a458aab8c23865
Tamago <keiichiro.tany@gmail.com>
2021-03-31 14:12:28 +0000

    difficult redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,47 @@
 // yarn add redux react-redux
 
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+import { increment, decrement } from '../actions';
 
 // actionの作成
 // action = appの中で何が起きたかを示す
 
-
-// index.js:1 Warning: Each child in a list should have a unique "key" prop.
-// このwarningみたらkey={index}を足してあげる
-const App = () => {
-  return (
-    <Counter></Counter>
-  )
-}
 
 // Propsは親のコンポーネントから値を渡されたのに対して
 // stateはコンポーネントの内部でのみ利用される
 // propsは変更不可能,stateは変更可能
 
 // class書いて、method書いて,最後にrender書いて返す
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    console.log(this.state)
-    this.state = {count : 0}
-  }
-
-  // stateの変更にはsetStateがお決まりのように使う
-  // setStateが呼び出されると自動でrenderされる
-  handlePlusBottun = () => {
-    console.log("handlePlusBottun")
-    this.setState({count : this.state.count + 1})
-  }
-
-  handleMinusBottun = () => {
-    console.log("handleMinusBottun")
-    this.setState({count : this.state.count - 1})
-  }
+class App extends Component {
+  // constractorはreduxで処理するから削除
+  // constructor(props) {
+  //   super(props)
+  //   console.log(this.state)
+  //   this.state = {count : 0}
+  // }
 
   render(){
-    console.log("render")
+    const props = this.props
     return(
     <React.Fragment>
-      <div>count: {this.state.count}</div>
-      <button onClick={this.handlePlusBottun}>+1</button>
-      <button onClick={this.handleMinusBottun}>-1</button>
+      <div>value: {props.value}</div>
+      <button onClick={props.increment}>+1</button>
+      <button onClick={props.decrement}>-1</button>
     </React.Fragment>
     )
   }
 }
 
+const mapStateToProps = state => ({ value: state.count.value })
+const mapDispatchToProps = dispatch => ({ 
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
 
 
-export default App;
+//export default App;
