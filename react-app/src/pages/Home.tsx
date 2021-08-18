1924c95eefb6733fcfe61c3176fa5028c5128935
Tamago <keiichiro.tany@gmail.com>
2021-08-18 13:30:18 +0000

    title

--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
+import '../css/Home.css'
+
 const Home = () => {
     return (
-        <div className='home'>
-            <h1>Home</h1>
-        </div>
+        <>
+
+            <div className='home-title'>
+                <h1>機械学習手法の選択</h1>
+            </div>
+
+            <section className='home-kind'>
+                <h2>教師あり学習</h2>
+                <p> aaaa </p>
+            </section>
+
+            <section className='home-kind'>
+                <h2>教師なし学習</h2>
+                <p> iiii </p>
+            </section>
+
+
+        </>
     )
 }
 
