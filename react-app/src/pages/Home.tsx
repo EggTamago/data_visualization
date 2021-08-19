95f1ebeee8d9f5e5b577ed69457679ead0e5f472
Tamago <keiichiro.tany@gmail.com>
2021-08-19 14:31:16 +0000

    add some card

--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -1,37 +1,81 @@
-import React from 'react'
+import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Grid, { GridSpacing } from '@material-ui/core/Grid';
 
 import '../css/Home.css'
 
-const Home = () => {
-    return (
-        <>
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+});
 
+const Home = () => {
+
+    const classes = useStyles();
+
+    return (
+
+        <>
             <div className='home-title'>
                 <h1>機械学習手法の選択</h1>
             </div>
 
-            <section className='home-kind'>
-                <p>DB connection for big date</p>
-                <p>light data is no need db connection because process in only API server</p>
-                <p>data upload fuinction</p>
-                <p>show data like table with static</p>
-                <p>some graph</p>
-                <p>some analysis and create model file</p>
-            </section>
+            <Card className={classes.root}>
+                <CardActionArea>
 
-            <section className='home-kind'>
-                <h2>教師あり学習</h2>
-                <p> aaaa </p>
-            </section>
-
-            <section className='home-kind'>
-                <h2>教師なし学習</h2>
-                <p> iiii </p>
-            </section>
-
-
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            単回帰分析
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            一番簡単な線形回帰
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Share
+                    </Button>
+                    <Button size="small" color="primary">
+                        Learn More
+                    </Button>
+                </CardActions>
+            </Card>
         </>
-    )
+    );
 }
+
+
+//             <section className='home-kind'>
+//                 <p>DB connection for big date</p>
+//                 <p>light data is no need db connection because process in only API server</p>
+//                 <p>data upload fuinction</p>
+//                 <p>show data like table with static</p>
+//                 <p>some graph</p>
+//                 <p>some analysis and create model file</p>
+//             </section>
+
+//             <section className='home-kind'>
+//                 <h2>教師あり学習</h2>
+//                 <p> aaaa </p>
+//             </section>
+
+//             <section className='home-kind'>
+//                 <h2>教師なし学習</h2>
+//                 <p> iiii </p>
+//             </section>
+
+
+//         </>
+//     )
+// }
 
 export default Home
